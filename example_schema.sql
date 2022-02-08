0ffd729eb724e177c8e02181006481032de62144
Alexey <shaymanov@inbox.ru>
2022-02-08 20:22:54 +0300

    create table of places

--- example_schema.sql
+++ example_schema.sql
@@ -1,7 +1,17 @@
 drop table if exists examples;
+drop table if exists places;
 
 create table `examples` (
   `id` int not null auto_increment,
   `name` varchar(80) default null,
   primary key (`id`)
 );
+
+create table `places` (
+   `id` int not null auto_increment,
+   `city` varchar(80) default null,
+   `county` varchar(80) default null,
+   `country` varchar(80) default null,
+   primary key (`id`)
+);	
+
--- images/example-python/example.py
+++ images/example-python/example.py
@@ -3,6 +3,7 @@
 import csv
 import json
 import sqlalchemy
+import pandas as pd
 
 # connect to the database
 engine = sqlalchemy.create_engine("mysql://codetest:swordfish@database/codetest")
@@ -12,16 +13,30 @@ metadata = sqlalchemy.schema.MetaData(engine)
 
 # make an ORM object to refer to the table
 Example = sqlalchemy.schema.Table('examples', metadata, autoload=True, autoload_with=engine)
+Places = sqlalchemy.schema.Table('places', metadata, autoload=True, autoload_with=engine)
+print(*connection.execute(sqlalchemy.sql.select([Places])).keys())
 
+def generate_out_file(table, out_name):
+  # output the table to a JSON file
+  print("write report")
+  columns = connection.execute(sqlalchemy.sql.select([Places])).keys()
+  with open(out_name, 'w') as json_file:
+    rows = connection.execute(sqlalchemy.sql.select([table])).fetchall()
+    rows = [dict(zip(columns, row)) for row in rows]
+    json.dump(rows, json_file, separators=(',', ':'))
+
+def write_to_sql_table(data_in, table):
+  print("write to table")
+  df = pd.read_csv(data_in)
+  print(df)
+  with open(data_in) as csv_file:
+    reader = csv.reader(csv_file)
+    next(reader)
+    for row in reader:
+      connection.execute(table.insert().values(**dict(zip(df.columns, row))))
+
+write_to_sql_table('/data/places.csv', Places)
+write_to_sql_table('/data/example.csv', Example)
+generate_out_file(Places, '/data/places_json.json')
 # read the CSV data file into the table
-with open('/data/example.csv') as csv_file:
-  reader = csv.reader(csv_file)
-  next(reader)
-  for row in reader: 
-    connection.execute(Example.insert().values(name = row[0]))
 
-# output the table to a JSON file
-with open('/data/example_python.json', 'w') as json_file:
-  rows = connection.execute(sqlalchemy.sql.select([Example])).fetchall()
-  rows = [{'id': row[0], 'name': row[1]} for row in rows]
-  json.dump(rows, json_file, separators=(',', ':'))
